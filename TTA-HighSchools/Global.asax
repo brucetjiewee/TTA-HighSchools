6c01811a9f906020ea8c0d783346f1ca015e3b57
unknown <brucetjie.wee@gmail.com>
2018-12-12 22:55:23 +0200

    initial commit

---
+++ TTA-HighSchools/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TTA_HighSchools.MvcApplication" Language="C#" %>
---
+++ TTA-HighSchools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TTA_HighSchools.Startup))]
+namespace TTA_HighSchools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
